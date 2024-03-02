d4578dcc11ac5c95b7286ad36acd0c57a0c59fd9
David Hoover <me@hooverd.com>
2024-03-02 13:27:01 -0600

    Add helper script.

---
+++ backend/up.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export WEBAUTHN_RP_ID="passkeys-demo.hooverd.com"
+export WEBAUTHN_RP_NAME="Passkeys Demo"
+export WEBAUTHN_ORIGIN="https://passkeys-demo.hooverd.com"
+
+gunicorn -w "4" -b "127.0.0.1:8080" "src.app:app"

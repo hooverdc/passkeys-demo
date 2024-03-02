9995cb47218ebe420efdb04200fe4ed9ca314afd
David Hoover <me@hooverd.com>
2024-03-02 13:33:14 -0600

    Run in background.

--- backend/up.sh
+++ backend/up.sh
@@ -4,4 +4,4 @@ export WEBAUTHN_RP_ID="passkeys-demo.hooverd.com"
 export WEBAUTHN_RP_NAME="Passkeys Demo"
 export WEBAUTHN_ORIGIN="https://passkeys-demo.hooverd.com"
 
-gunicorn -w "4" -b "127.0.0.1:8080" "src.app:app"
+gunicorn -w "4" -b "127.0.0.1:8080" "src.app:app" --daemon
